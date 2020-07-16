90d02d69dffa4bb6593a041a16d887ba48d8c0fc
Murad Cafarov <cafarovmurad9@gmail.com>
2020-07-16 19:04:59 +0400

    SlugProject

---
+++ SlugTestApp/SlugTestApp/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SlugTestApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SlugTestApp.Controllers
+{
+    public class UsersController : Controller
+    {
+        private static List<User> GetUsers;
+        public UsersController()
+        {
+            GetUsers = new List<User>
+            {
+                new User{Id = 1,Name = "Murad",Desc = "Ela oglandi"},
+                new User{Id = 2,Name = "Ilham",Desc = "Lap Ela oglandi"},
+                new User{Id = 3,Name = "Orxan",Desc = "Lap Lap Ela oglandi"}
+            };
+        }
+        [Route("butunUserler")]
+        [Route("allUsers")]
+        [Route("users")]
+        [Route("/")]
+        public ActionResult GetAllUsers()
+        {
+            var data = GetUsers;
+            return View(data);
+        }
+        public IActionResult GetById(int id)
+        {
+            var data = GetUsers.FirstOrDefault(i => i.Id == id);
+            return View(data);
+        }
+    }
+}
---
+++ SlugTestApp/SlugTestApp/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SlugTestApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Desc { get; set; }
+    }
+}
