a3efceb622173b2507333ae924fc7773b19a4575
Murad Cafarov <cafarovmurad9@gmail.com>
2020-08-09 15:37:18 +0400

    Fix

--- ToDoApp/ToDoApp/Controllers/HomeController.cs
+++ ToDoApp/ToDoApp/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ToDoApp.Models;
 
@@ -10,6 +13,13 @@ namespace ToDoApp.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IHostingEnvironment hostingEnvironment;
+
+        public HomeController(IHostingEnvironment hostingEnvironment)
+        {
+            this.hostingEnvironment = hostingEnvironment;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -20,6 +30,23 @@ namespace ToDoApp.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult UploadImage(IFormFile file)
+        {
+            var uploads = Path.Combine(hostingEnvironment.WebRootPath + "/uploads");
+            var fileName = Guid.NewGuid().ToString() + file.FileName.ToString();
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), uploads, fileName);
+
+            file.CopyTo(new FileStream(filePath, FileMode.OpenOrCreate));
+
+
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register()
 
         public IActionResult Privacy()
         {
---
+++ ToDoApp/ToDoApp/Models/RegisterForm.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Models
+{
+    public class RegisterForm
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Pass { get; set; }
+        public IFormFile File { get; set; }
+    }
+}
